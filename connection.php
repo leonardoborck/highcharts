192db39580b313798b9c650c61358b5fd93c9d6d
Leonardo Borck <l.borck@edu.univali.br>
2021-06-11 11:24:02 -0300

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,31 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "yourpass";
+$dbname = "test";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM test";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+	  $arr = array(
+		'name' => $row['name'],
+		'data' => array_map('intval',explode(',',$row['data']))
+	  );
+	  $series_array[] = $arr;
+  }
+  return json_encode($series_array);
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
